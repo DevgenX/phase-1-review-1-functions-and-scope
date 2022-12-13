ce53b19a69e066becedc447b0127b7d9cfdcf503
devgenx <sebastian110420@gmail.com>
2022-12-13 09:52:50 -0500

    Completed my solution

--- functionsAndScope.js
+++ functionsAndScope.js
@@ -3,6 +3,11 @@
 /*
  * Your code goes here
  */
+const sum = (num) => num + num;
+
+// function sum(num) {
+//   return num + num;
+// }
 
 // Deliverable # 2 - Write the code to invoke (call) your "sum" function.
 
@@ -10,35 +15,41 @@
  * Your code goes here
  */
 
+console.log(sum(10));
+
 /* Exercise # 1 - Block Scope: The while loop as seen below contains a block of code.
  * A variable named "day" has been declared and assigned the value of "Monday" inside of this while loop.
  * What happens when you try to access the value of this variable outside of the while loop?
  */
-let time = 10
-while(time > 0){
-    let day = "Monday"
-    console.log(`Tick tock! ${time} seconds left`)
-    time--
+let time = 10;
+while (time > 0) {
+  let day = "Monday";
+  console.log(`Tick tock! ${time} seconds left`);
+  time--;
 }
-console.log("Time is up!")
+console.log("Time is up!");
 
 // Print out the value of the "day" variable here using console.log(), and notice what happens in the console.
+//console.log(day);
 
 // Exercise # 2 - Function Scope: What happens when you try to console log the value of "iceCreamFlavor" inside the "createIceCream" function vs outside of the function?
-let iceCreamFlavor = "Strawberry"
-function createIceCream(iceCreamFlavor){
-    // Print out the value of the "iceCreamFlavor" variable here using console.log(), and notice what happens in the console.
+let iceCreamFlavor = "Strawberry";
+function createIceCream(iceCreamFlavor) {
+  console.log(iceCreamFlavor);
+  // Print out the value of the "iceCreamFlavor" variable here using console.log(), and notice what happens in the console.
 }
-createIceCream("Matcha")
+createIceCream("Matcha");
 // Print out the value of the "iceCreamFlavor" variable here using console.log(), and notice what happens in the console.
 
 // Exercise # 3 - Global Scope: What happens when you create a variable in global scope and change that variable's value inside of a function or block of code?
-let pasta = "Capellini"
-function changePasta(){
-    pasta = "Linguini"
+let pasta = "Capellini";
+function changePasta() {
+  pasta = "Linguini";
 }
-changePasta()
+console.log(pasta);
+changePasta();
 // What will the value of "pasta" be now? Print out the value of the "pasta" variable using console.log() and find out!
+console.log(pasta);
 
 /* Deliverable # 3:
  * Step 1 - Declare a function named "guacamoleMaker" that accepts a function as its parameter.
@@ -50,7 +61,14 @@ changePasta()
 /*
  * Your code goes here
  */
+const guacamoleMaker = (func) => {
+  mixIngredients();
+  console.log("All done!");
+};
 
+const mixIngredients = () => console.log("Mixing Ingredients");
+
+guacamoleMaker(mixIngredients);
 /* Deliverable # 4:
  * Step 1 - Declare a function named "playASport".
  * Step 2 - Inside of the "playASport" function, declare a function named "playSoccer" that prints the string "GOAL!!!" to the console.
@@ -61,3 +79,9 @@ changePasta()
 /*
  * Your code goes here
  */
+
+const playASport = () => {
+  return (playSoccer = () => console.log("GOAL!!!"));
+};
+
+playASport();
